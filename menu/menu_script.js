e3be47fd5ea5f8c8ea7b378ab1704145e4e5b77c
nugugae <89291329+nugugae@users.noreply.github.com>
2022-09-07 02:16:18 +0900

    full_update

--- menu/menu_script.js
+++ menu/menu_script.js
@@ -1,17 +1,15 @@
-m1.style.display = "none";
-m2.style.display = "none";
-m3.style.display = "none";
-m4.style.display = "none";
+menuBar.style.display = "none";
 
-function showMenu1(){
-    document.getElementById("m1").style.display = "inline-block";
+function hidemenuBar(){
+    document.getElementById("menuBar").style.display = "inline-block";
 }
-function showMenu2(){
-    document.getElementById("m2").style.display = "inline-block";
-}
-function showMenu3(){
-    document.getElementById("m3").style.display = "inline-block";
-}
-function showMenu4(){
-    document.getElementById("m4").style.display = "inline-block";
-}
+
+const isMobile = () => {
+    try {
+      document.createEvent("TouchEvent");
+      return true;
+    } catch (e) {
+      return false;
+    }
+};
+  
